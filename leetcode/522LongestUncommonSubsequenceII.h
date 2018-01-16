8898838f68c60cf5eaffb4307c9723016a26af82
lailianqi <lailianqi@gmail.com>
2018-01-16 21:18:22 +0800

    --- 添加 522题

---
+++ leetcode/522LongestUncommonSubsequenceII.h
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+using namespace std;
+// 这题没有写出来 直接看的leetcode的答案
+// https://discuss.leetcode.com/topic/85030/hashmap-sort-solution-c
+class Solution {
+  public:
+    int findLUSlength(vector<string> &strs) {}
+};

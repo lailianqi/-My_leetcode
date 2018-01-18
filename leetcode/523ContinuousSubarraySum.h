f0613cff50ecbb5f82fa4798f2c3fe4113097cc6
lailianqi <lailianqi@gmail.com>
2018-01-18 18:13:26 +0800

    --- 添加523新方案

--- leetcode/523ContinuousSubarraySum.h
+++ leetcode/523ContinuousSubarraySum.h
@@ -27,3 +27,22 @@ class Solution {
         return false;
     }
 };
+
+// 更加短的一个方法 来自leetcode的答案
+// https://discuss.leetcode.com/topic/80892/concise-c-solution-use-set-instead-of-map
+class Solution_0 {
+  public:
+    bool checkSubarraySum(vector<int> &nums, int k) {
+        int n = nums.size(), sum = 0, pre = 0;
+        unordered_set<int> modk;
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            int mod = k == 0 ? sum : sum % k;
+            if (modk.count(mod))
+                return true;
+            modk.insert(pre);
+            pre = mod;
+        }
+        return false;
+    }
+};

0f97dbe722a87fbfad77d2456579fbbfcde107b6
lailianqi <lailianqi@gmail.com>
2018-01-23 19:30:53 +0800

    --- 增加535题

---
+++ leetcode/535.h
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+//莫名其妙的题
+class Solution {
+  public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) { return longUrl; }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) { return shortUrl; }
+};

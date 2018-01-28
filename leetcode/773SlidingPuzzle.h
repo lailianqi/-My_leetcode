a43eeb0ac2e7d85787f2ad5f74891fc9a022ba11
lailianqi <lailianqi@gmail.com>
2018-01-28 18:37:03 +0800

    --- 重新添加了773的解法

--- leetcode/773SlidingPuzzle.h
+++ leetcode/773SlidingPuzzle.h
@@ -1,4 +1,7 @@
+#include <algorithm>
 #include <map>
+#include <string>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 // 第69周的周赛
@@ -53,7 +56,7 @@ class Solution_0 {
             }
         }
     }
-    int encode(vector<vector<int>> &board) {
+    int encode(vector<vector<int>> &board) { //为了hash的使用 encode
         int code = 0;
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
@@ -68,4 +71,37 @@ class Solution_0 {
     vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     int min_move = INT_MAX;
     map<int, int> dic;
+};
+
+// 更加短的解法 超级的牛逼 来自LeetCode
+// https://discuss.leetcode.com/topic/118726/c-9-lines-dfs-with-pruning
+class Solution_1 {
+
+  public:
+    int slidingPuzzle(vector<vector<int>> &board) {
+        string s = to_string(board[0][0]) + to_string(board[0][1]) +
+                   to_string(board[0][2]) + to_string(board[1][0]) +
+                   to_string(board[1][1]) + to_string(board[1][2]);
+        helper(s, unordered_map<string, int>() = {}, s.find('0'), s.find('0'),
+               0);
+        return min_moves == INT_MAX ? -1 : min_moves;
+    }
+    void helper(string s, unordered_map<string, int> &m, int cur_zero,
+                int swap_zero, int cur_move) {
+        swap(s[cur_zero], s[swap_zero]);
+        if (s == "123450")
+            min_moves = min(min_moves, cur_move);
+        if (cur_move < min_moves && (m[s] > cur_move || m[s] == 0)) {
+            m[s] = cur_move;
+            for (auto new_zero : moves[swap_zero]) {
+                helper(s, m, swap_zero, new_zero, cur_move + 1);
+            }
+        }
+    }
+
+  private:
+    unordered_map<int, vector<int>> moves{
+        {0, {1, 3}},    {1, {0, 2, 4}}, {2, {1, 5}}, {3, {0, 4}},
+        {4, {3, 5, 1}}, {5, {4, 2}}}; //字符串变成一维是需要走的位置
+    int min_moves = INT_MAX;
 };
